0ab32fa7fbb4d7d6b60673581d17a69edc03e2ea
Evin Kazalak <79329805+Evinkazalak@users.noreply.github.com>
2024-12-08 20:37:54 +0300

    Add files via upload

---
+++ Class8.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Class1
+{
+// Eğitsel Geri Bildirimlerle Görsel/İşitsel Destek*
+// Kod (Doğru/Yanlış Cevapların Geri Bildirimi):
+    
+
+public class FeedbackManager : MonoBehaviour
+{
+    public Text feedbackText;
+
+    public void ShowFeedback(bool isCorrect)
+    {
+        if (isCorrect)
+        {
+            feedbackText.text = "Tebrikler! Bu bir kare oldu!";
+            feedbackText.color = Color.green;
+        }
+        else
+        {
+            feedbackText.text = "Tekrar deneyin!";
+            feedbackText.color = Color.red;
+        }
+
+        Invoke("ClearFeedback", 2f);  // 2 saniye sonra geri bildirimi temizle
+    }
+
+    void ClearFeedback()
+    {
+        feedbackText.text = "";
+    }
+}
+public Class1()
+	{
+	}
+}

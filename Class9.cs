5fcc6b597da26c68b383ea8df23da13e52cd20f5
Evin Kazalak <79329805+Evinkazalak@users.noreply.github.com>
2024-12-08 20:38:12 +0300

    Add files via upload

---
+++ Class9.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Class1
+{
+//Oyun Mekanikleri*
+// Ödül Sistemi
+  
+
+
+public class ScoreManager : MonoBehaviour
+{
+    public int score = 0;
+    public Text scoreText;
+
+    public void AddScore(int points)
+    {
+        score += points;
+        UpdateScoreUI();
+    }
+
+    void UpdateScoreUI()
+    {
+        scoreText.text = "Puan: " + score;
+    }
+}
+
+//Her doğru cevapta:
+
+FindObjectOfType<ScoreManager>(9,12).AddScore(10); 
+	{
+	}
+}

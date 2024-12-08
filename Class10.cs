52f676eab25a640b49e4fbb0cb42ac90f0196224
Evin Kazalak <79329805+Evinkazalak@users.noreply.github.com>
2024-12-08 20:38:31 +0300

    Add files via upload

---
+++ Class10.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+
+public class Class1
+{
+    //Renk İpuçları
+    // Kırmızı ve mavi renklerde ipuçları ekleyerek çocukların rehberlik almasını  sağlarsın.
+
+
+
+
+    public class ColorHint : MonoBehaviour
+    {
+        public SpriteRenderer shapeRenderer;
+
+        public void HighlightShape()
+        {
+            shapeRenderer.color = Color.blue;  // Örneğin mavi ipucu
+            Invoke("ResetColor", 1f);
+        }
+
+        void ResetColor()
+        {
+            shapeRenderer.color = Color.white;  // Rengi varsayılan renge döndür
+        }
+    }
+
+}
+

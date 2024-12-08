3eb2b2289ff326763a6a3d649f253c9e0657de5d
Evin Kazalak <79329805+Evinkazalak@users.noreply.github.com>
2024-12-08 20:37:11 +0300

    Add files via upload

---
+++ Class6.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+public class Class1
+{
+    //Animator’da bir *Trigger* ekleyin ve* Success* olarak adlandırın.Bu, doğru eşleşme durumunda animasyonu oynatır.
+    
+
+
+//### Ses Efektleri
+//Ses efektlerini eklemek için:
+//. *AudioClip* ekle  (ör.alkış sesi)
+
+public class AudioFeedback : MonoBehaviour
+{
+    public AudioClip successSound;
+    public AudioClip levelChangeSound;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlaySuccessSound()
+    {
+        audioSource.clip = successSound;
+        audioSource.Play();
+    }
+
+    public void PlayLevelChangeSound()
+    {
+        audioSource.clip = levelChangeSound;
+        audioSource.Play();
+    }
+}
+public Class1()
+	{
+	}
+}

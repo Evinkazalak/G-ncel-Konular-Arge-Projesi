f15a519bc63769027d9a4de93d595e3b78eb58ed
Evin Kazalak <79329805+Evinkazalak@users.noreply.github.com>
2024-12-08 20:37:32 +0300

    Add files via upload

---
+++ Class7.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+public class Class1
+{
+// Dinamik Zorluk Artırımı*
+//Zorluk seviyelerini kontrol etmek için seviyelere göre prefab şekiller ile oluşturulur.
+
+public class DifficultyManager : MonoBehaviour
+{
+    public ShapeLoader shapeLoader;  // Şekilleri yükleyen sınıf
+    public int currentLevel = 1;
+
+    public void AdjustDifficulty()
+    {
+        if (currentLevel <= 3)
+        {
+            shapeLoader.LoadRandomShape();  // Basit şekiller
+        }
+        else if (currentLevel <= 5)
+        {
+            // Daha karmaşık şekiller için yeni prefab'ler ekleyin
+            shapeLoader.LoadRandomShape();
+        }
+        else
+        {
+            // Çokgenler veya desenler
+            shapeLoader.LoadRandomShape();
+        }
+    }
+
+    public void LevelUp()
+    {
+        currentLevel++;
+        AdjustDifficulty();
+    }
+}
+public Class1()
+	{
+	}
+}

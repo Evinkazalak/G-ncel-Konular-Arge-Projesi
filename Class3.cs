641942dd5c89b01fda432bc90c820d007aee2284
Evin Kazalak <79329805+Evinkazalak@users.noreply.github.com>
2024-12-08 20:36:11 +0300

    Add files via upload

---
+++ Class3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class Class1
+{
+    public Class1()
+    {
+
+    }
+//ARRaycastManager Kullanımı
+
+
+
+    public class ShapePlacement : MonoBehaviour
+    {
+        public GameObject shapePrefab;
+        private ARRaycastManager raycastManager;
+
+        void Start()
+        {
+            raycastManager = FindObjectOfType<ARRaycastManager>();
+        }
+
+        void Update()
+        {
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+                if (touch.phase == TouchPhase.Began)
+                {
+                    PlaceShape(touch.position);
+                }
+            }
+        }
+        void PlaceShape(Vector2 touchPosition)
+        {
+            List<ARRaycastHit> hits = new List<ARRaycastHit>();
+            if (raycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
+            {
+                Pose hitPose = hits[0].pose;
+                Instantiate(shapePrefab, hitPose.position, hitPose.rotation);
+            }
+        }
+    }
+
+}
+

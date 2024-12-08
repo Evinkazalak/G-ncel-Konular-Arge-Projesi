8f0f9da267ae3e5493e48051d5bfc7d55ac0c05b
Evin Kazalak <79329805+Evinkazalak@users.noreply.github.com>
2024-12-08 20:35:49 +0300

    Add files via upload

---
+++ Class2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+	public Class1()
+    {
+
+    }
+	
+	
+
+  
+//Çocuklara görsel ve sesli geri bildirim sağlamak için kullanıcı arayüzünü yönetir.
+
+
+
+
+    public Text feedbackText;
+    public GameObject nextLevelButton;
+
+    public void ShowFeedback(string message, Color color)
+    {
+        feedbackText.text = message;
+        feedbackText.color = color;
+        feedbackText.gameObject.SetActive(true);
+
+        // Birkaç saniye sonra geri bildirimi gizle
+        Invoke("HideFeedback", 2f);
+    }
+
+    void HideFeedback()
+    {
+        feedbackText.gameObject.SetActive(false);
+    }
+
+    public void ShowNextLevelButton()
+    {
+        nextLevelButton.SetActive(true);
+    }
+
+    public void OnNextLevelButtonClicked()
+    {
+        // Yeni bir zorluk seviyesi oluştur
+        nextLevelButton.SetActive(false);
+        FindObjectOfType<ShapeManager>().GenerateShape();
+    }
+}

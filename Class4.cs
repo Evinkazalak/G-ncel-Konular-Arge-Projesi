b3665ba68f7d010152f2ddb10187dc624a2ce3b3
Evin Kazalak <79329805+Evinkazalak@users.noreply.github.com>
2024-12-08 20:36:31 +0300

    Add files via upload

---
+++ Class4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+
+public class Class1
+{
+   // Şekil Seçici Kod(Random Shape Loader) :*
+
+
+public class ShapeLoader : MonoBehaviour
+{
+    public GameObject[] halfShapes;  // Kırmızı ve mavi yarım şekiller
+    public Transform spawnPoint;
+
+    public void LoadRandomShape()
+    {
+        int randomIndex = Random.Range(0, halfShapes.Length);
+        Instantiate(halfShapes[randomIndex], spawnPoint.position, Quaternion.identity);
+    }
+}
+public Class1()
+	{
+	}
+}

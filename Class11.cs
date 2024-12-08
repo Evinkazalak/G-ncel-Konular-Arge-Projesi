39f21278cfdfedcb584c87305eb480f4c4cae8b2
Evin Kazalak <79329805+Evinkazalak@users.noreply.github.com>
2024-12-08 20:38:52 +0300

    Add files via upload

---
+++ Class11.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+}
+//Genel Akış*
+   //1. * Başlangıç*: İlk seviyede basit şekiller yüklenecek.
+//Çizim Kontrolü*: Şekiller tamamlandığında geri bildirim(ses ve yazı) sağlanacak.
+// Seviye İlerlemesi*: Zorluk artarken şekiller karmaşık hale gelecek.
+ //Animasyon ve Ödüller*: Doğru cevapta animasyon ve ödül puanı verilecek.**

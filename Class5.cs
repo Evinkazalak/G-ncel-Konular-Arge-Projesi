f011264b6537cad96cfaf27456c48fa9b17789e0
Evin Kazalak <79329805+Evinkazalak@users.noreply.github.com>
2024-12-08 20:36:51 +0300

    Add files via upload

---
+++ Class5.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class Class1
+{
+   // Kod(Animasyonu Tetikleme) :*
+
+
+
+public class ShapeAnimator : MonoBehaviour
+{
+    public Animator shapeAnimator;
+
+    public void PlaySuccessAnimation()
+    {
+        shapeAnimator.SetTrigger("Success");
+    }
+}
+public Class1()
+	{
+	}
+}
